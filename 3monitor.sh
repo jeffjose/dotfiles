2324c7c1a45614d6304d5fbeda486470f3c12541
Jeffrey Jose <jeffjosejeff@gmail.com>
2015-10-15 00:27:16 -0400

    softlink -> hardlink

--- 3monitor.sh
+++ 3monitor.sh
@@ -1,1 +1,2 @@
-/home/jeffjose/.screenlayout/3monitor.sh
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 2160x0 --rotate normal --output DP1 --off --output DP2-1 --mode 1920x1080 --pos 1080x0 --rotate right --output DP2-2 --mode 1920x1080 --pos 0x0 --rotate left --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
