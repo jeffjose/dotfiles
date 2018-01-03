eecdf841a40cb66cf3ad9ed7e60b3831ab256796
Jeffrey Jose <jeffjosejeff@gmail.com>
2018-01-02 17:28:05 -0800

    adding comments and some setup for vim plugins

--- setup.sh
+++ setup.sh
@@ -1,32 +1,49 @@
 #!/usr/bin/tcsh -fb
 
-mkdir -p $HOME/scripts
-mkdir -p $HOME/Downloads
-mkdir -p $HOME/.config/nvim
-mkdir -p $HOME/.screen
+# Setting up some directories
+#
+#
+  mkdir -p $HOME/scripts
+  mkdir -p $HOME/Downloads
+  mkdir -p $HOME/.config/nvim
+  mkdir -p $HOME/.screen
 
-touch $HOME/.aliases.sensitive.sh
+# Ensure that the .aliases.sensitive.sh file exists
+#
+#
+  touch $HOME/.aliases.sensitive.sh
 
-ln .aliases                    $HOME/.aliases
-ln .aliases.bash               $HOME/.aliases.bash
-ln .aliases.google.sh          $HOME/.aliases.google.sh
-ln .colordiffrc                $HOME/.colordiffrc
-ln .cshrc                      $HOME/.cshrc
-ln .cwdcmd                     $HOME/.cwdcmd
-ln .gitconfig                  $HOME/.gitconfig
-ln .pdbrc                      $HOME/.pdbrc
-ln .precmd                     $HOME/.precmd
-ln .prompt                     $HOME/.prompt
-ln .screen/default             $HOME/.screen/default
-ln .screenrc                   $HOME/.screenrc
-ln .tabletaliases              $HOME/.tabletaliases
-ln .vimrc                      $HOME/.vimrc
-ln .XResources                 $HOME/.XResources
+# Setup the essential vim plugins
+#
+#
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-ln updateall.sh $HOME/scripts/updateall.sh
+# Link dotfiles
+#
+#
+  ln .aliases                    $HOME/.aliases
+  ln .aliases.bash               $HOME/.aliases.bash
+  ln .aliases.google.sh          $HOME/.aliases.google.sh
+  ln .colordiffrc                $HOME/.colordiffrc
+  ln .cshrc                      $HOME/.cshrc
+  ln .cwdcmd                     $HOME/.cwdcmd
+  ln .gitconfig                  $HOME/.gitconfig
+  ln .pdbrc                      $HOME/.pdbrc
+  ln .precmd                     $HOME/.precmd
+  ln .prompt                     $HOME/.prompt
+  ln .screen/default             $HOME/.screen/default
+  ln .screenrc                   $HOME/.screenrc
+  ln .tabletaliases              $HOME/.tabletaliases
+  ln .vimrc                      $HOME/.vimrc
+  ln .XResources                 $HOME/.XResources
 
-ln init.vim $HOME/.config/nvim/init.vim
+  ln updateall.sh $HOME/scripts/updateall.sh
 
+  ln init.vim $HOME/.config/nvim/init.vim
+
+# Ignore
+#
 #ccsm.profile
 #ccsm.with.defaults.profile
 #gradle.properties
