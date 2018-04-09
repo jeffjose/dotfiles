8e8d699c587a0e82bb3ec0518148eee7acd27365
Jeffrey Jose <jeffjosejeff@gmail.com>
2018-04-08 18:35:52 -0700

    remove vundle

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@
 # Setup the essential vim plugins
 #
 #
+  rm -rf ~/.vim/bundle/Vundle.vim
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
   mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
