6f265aa255dad9134a1b17a8911735d5142eba92
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-09-21 21:17:06 -0700

    adding ncdu, htop, qbittorrent and tget

--- firstrun.sh
+++ firstrun.sh
@@ -1,4 +1,4 @@
-sudo apt install git neovim curl moreutils tilix ack screen gitk
+sudo apt install git neovim curl moreutils tilix ack screen gitk ncdu htop qbittorrent
 
 mkdir -p ~/bin/
 ln /usr/bin/screen ~/bin/scrn -s
@@ -6,7 +6,10 @@ ln /usr/bin/screen ~/bin/scrn -s
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
+# Yarn
 sudo apt-get update && sudo apt-get install yarn
+yarn global add t-get
+
 
 sudo apt autoremove
 
