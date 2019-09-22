163c2d3e9f86f17f16a6130dd053d34e2a20a8e3
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-09-21 20:26:12 -0700

    adding firstrun

---
+++ firstrun.sh
@@ -0,0 +1,11 @@
+sudo apt install git neovim curl moreutils tilix ack screen gitk
+
+mkdir -p ~/bin/
+ln /usr/bin/screen ~/bin/scrn -s
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update && sudo apt-get install yarn
+
+sudo apt autoremove
