127cd850783f8fb6767b8cdd917bb30df8b1fd7f
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-09-22 07:40:17 -0700

    docker

--- firstrun.sh
+++ firstrun.sh
@@ -1,15 +1,35 @@
+#!/usr/bin/bash
+#
+# Jeffrey Jose
+#
+
+# Packages
 sudo apt install -y git neovim curl moreutils tilix ack screen gitk ncdu htop qbittorrent tcsh
 
 mkdir -p ~/bin/
 ln /usr/bin/screen ~/bin/scrn -s
 
+# Install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-# Yarn
 sudo apt-get update && sudo apt-get -y install yarn
+
+# Yarn packages
 yarn global add t-get
 
+# Docker
+sudo apt-get -y remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
 
 sudo apt autoremove
 
