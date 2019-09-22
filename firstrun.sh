240b59c151170ded320af5095594906bce2c5656
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-09-22 05:27:10 -0700

    non-interactive and tcsh

--- firstrun.sh
+++ firstrun.sh
@@ -1,4 +1,4 @@
-sudo apt install git neovim curl moreutils tilix ack screen gitk ncdu htop qbittorrent
+sudo apt install -y git neovim curl moreutils tilix ack screen gitk ncdu htop qbittorrent tcsh
 
 mkdir -p ~/bin/
 ln /usr/bin/screen ~/bin/scrn -s
@@ -7,7 +7,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 # Yarn
-sudo apt-get update && sudo apt-get install yarn
+sudo apt-get update && sudo apt-get -y install yarn
 yarn global add t-get
 
 
