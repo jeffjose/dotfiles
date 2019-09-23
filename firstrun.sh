a060ecc0bffc7a859d1ae2a8e2b2ab260940432c
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-09-22 20:48:54 -0700

    separating out docker to its own file

--- firstrun.sh
+++ firstrun.sh
@@ -17,20 +17,8 @@ sudo apt-get update && sudo apt-get -y install yarn
 # Yarn packages
 yarn global add t-get
 
-# Docker
-sudo apt-get -y remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-
 sudo apt autoremove
 
-#ssh-keygen -t rsa -b 4096 -C "jeffjosejeff@gmail.com"
+ssh-keygen -t rsa -b 4096 -C "jeffjosejeff@gmail.com"
+ssh-add ~/.ssh/id_rsa
+
---
+++ install-docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+#
+# Jeffrey Jose
+#
+# Docker
+sudo apt-get -y remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+sudo groupadd docker 2> /dev/null
+sudo usermod -aG docker $USER
+sudo newgrp docker
