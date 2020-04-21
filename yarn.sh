8c771af38a389e52e1a5eec53986280ee7e0eab3
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-04-20 22:53:49 -0700

    vscode material theme

--- yarn.sh
+++ yarn.sh
@@ -5,6 +5,9 @@
 
 yarn global add t-get yarn-recursive npx coffeescript less firebase firebase-tools peerflix gulp-cli
 
+#  To create vscode theme from .tmtheme
+yarn global yo generator-code
+
 yarn global add prettier
 yarn global add @prettier/plugin-php
 yarn global add @prettier/plugin-pug
