c91b8dd0c351ba5593c958302acb77dce95a5ddb
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-04-20 22:17:41 -0700

    adding prettier plugins

--- yarn.sh
+++ yarn.sh
@@ -3,4 +3,10 @@
 # Jeffrey Jose | Jan 06, 2020
 #
 
-yarn global add t-get yarn-recursive npx prettier coffeescript less prettier-plugin-svelte firebase firebase-tools peerflix gulp-cli @prettier/plugin-php
+yarn global add t-get yarn-recursive npx coffeescript less firebase firebase-tools peerflix gulp-cli
+
+yarn global add prettier
+yarn global add @prettier/plugin-php
+yarn global add @prettier/plugin-pug
+
+yarn global add prettier-plugin-svelte
