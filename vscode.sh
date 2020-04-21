b86fe161ef7ad0cd3e51f33c31a27e8326814858
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-04-20 22:47:07 -0700

    adding one more extension

--- vscode.sh
+++ vscode.sh
@@ -1,13 +1,18 @@
 # vim
 code --install-extension vscodevim.vim
 
-# tailwindcss: order class names
-code --install-extension heybourn.headwind
-
-# prettier
+# formatter: prettier
 code --install-extension esbenp.prettier-vscode
 
+# .svelte
+code --install-extension JamesBirtles.svelte-vscode
+
+# tailwindcss: order class names
+code --install-extension heybourn.headwind
 
 # Themes
 code --install-extension monokai.theme-monokai-pro-vscode
 code --install-extension PKief.material-icon-theme
+
+# Format all files
+code --install-extension lacroixdavid1.vscode-format-context-menu
