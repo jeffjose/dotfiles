8fcb2076a8df47b6b1783dceb588b0818dd40527
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-04-20 22:10:54 -0700

    vscode install

---
+++ vscode.sh
@@ -0,0 +1,13 @@
+# vim
+code --install-extension vscodevim.vim
+
+# tailwindcss: order class names
+code --install-extension heybourn.headwind
+
+# prettier
+code --install-extension esbenp.prettier-vscode
+
+
+# Themes
+code --install-extension monokai.theme-monokai-pro-vscode
+code --install-extension PKief.material-icon-theme
