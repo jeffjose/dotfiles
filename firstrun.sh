6c2b37de110db2e2a27d28f270106f0ba8e2bd72
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-09-23 00:40:52 -0700

    reordering

--- firstrun.sh
+++ firstrun.sh
@@ -46,6 +46,12 @@ sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | ap
 sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
 sudo apt-get update && sudo apt-get -y dart
 
+# Install gh (github CLI)
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install -y gh
+
 # Flutter
 sudo snap install flutter --classic
 
@@ -58,8 +64,3 @@ sudo apt autoremove
 ssh-keygen -t rsa -b 4096 -C "jeffjosejeff@gmail.com"
 ssh-add ~/.ssh/id_rsa
 
-# Install gh (github CLI)
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages
-sudo apt update
-sudo apt install -y gh
