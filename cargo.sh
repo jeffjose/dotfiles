14a602d1a0a2fbe53e15c8d911e44a8c5e906a7e
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-10-21 21:56:01 -0700

    cargo and install fonts

---
+++ cargo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Jeffrey Jose | Oct 21, 2020
+#
+
+echo "-----------------"
+echo "Cargo / Rust"
+echo "-----------------"
+
+cargo install timeago-cli
--- firstrun.sh
+++ firstrun.sh
@@ -46,6 +46,12 @@ curl -o- -L https://yarnpkg.com/install.sh | bash
 # VS Code
 ./vscode.sh
 
+# Cargo / Rust
+./cargo.sh
+
+# Fonts
+./fonts.sh
+
 # Install dart
 sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
 sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
