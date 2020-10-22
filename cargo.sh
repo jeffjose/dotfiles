98f707f9c2d9fa10f3f5310f4f790d39a09c1984
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-10-21 22:47:19 -0700

    force

--- cargo.sh
+++ cargo.sh
@@ -7,4 +7,8 @@ echo "-----------------"
 echo "Cargo / Rust"
 echo "-----------------"
 
+# For gitstatus (prompt)
 cargo install timeago-cli --force
+
+# For bumping versions
+cargo install cargo-bump --force
