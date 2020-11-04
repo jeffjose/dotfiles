172dad1c6a106af9e738c25f06104a031a84751b
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-11-03 17:32:04 -0800

    .xsession

--- .cshrc
+++ .cshrc
@@ -90,7 +90,7 @@ if ( ! $?prompt ) exit
     #
     # if Caps Lock gets stuck in "on" position, run `setxkbmap -option` to undo the following line
     #setxkbmap -option caps:ctrl_modifier
-    xmodmap ~/.xmodmap
+    #xmodmap ~/.xmodmap
 
 # Synaptics
 # This has to be done because my touchpad kinda broke after update to 15.10
--- setup.sh
+++ setup.sh
@@ -8,73 +8,95 @@ echo "-----------------"
 echo "Setting up $HOME"
 echo "-----------------"
 
-  mkdir -p $HOME/scripts
-  mkdir -p $HOME/Downloads
-  mkdir -p $HOME/.config/nvim
-  mkdir -p $HOME/.screen
-  mkdir -p $HOME/.config/Code/User/
-  mkdir -p $HOME/.config/catt/
-  #mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/
-  mkdir -p $HOME/.ipython/profile_default/
-  mkdir -p $HOME/.screenlayout
-  mkdir -p $HOME/.jupyter/custom/
+mkdir -p $HOME/scripts
+mkdir -p $HOME/Downloads
+mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.screen
+mkdir -p $HOME/.config/Code/User/
+mkdir -p $HOME/.config/catt/
+#mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/
+mkdir -p $HOME/.ipython/profile_default/
+mkdir -p $HOME/.screenlayout
+mkdir -p $HOME/.jupyter/custom/
 
-  touch /tmp/cwdcmd_recent_dirs
+touch /tmp/cwdcmd_recent_dirs
 
 # Ensure that the .aliases.sensitive.sh file exists
 #
 #
-  touch $HOME/.aliases.sensitive.sh
-
+touch $HOME/.aliases.sensitive.sh
 
 # Link dotfiles
 #
 #
-  rm $HOME/.aliases                                   ; ln .aliases                    $HOME/.aliases
-  rm $HOME/.aliases.bash                              ; ln .aliases.bash               $HOME/.aliases.bash
-  rm $HOME/.colordiffrc                               ; ln .colordiffrc                $HOME/.colordiffrc
-  rm $HOME/.cshrc                                     ; ln .cshrc                      $HOME/.cshrc
-  rm $HOME/.cwdcmd                                    ; ln .cwdcmd                     $HOME/.cwdcmd
-  rm $HOME/.gitconfig                                 ; ln .gitconfig                  $HOME/.gitconfig
-  rm $HOME/.pdbrc                                     ; ln .pdbrc                      $HOME/.pdbrc
-  rm $HOME/.precmd                                    ; ln .precmd                     $HOME/.precmd
-  rm $HOME/.prompt                                    ; ln .prompt                     $HOME/.prompt
-  rm $HOME/.screen/default                            ; ln .screen/default             $HOME/.screen/default
-  rm $HOME/.screenrc                                  ; ln .screenrc                   $HOME/.screenrc
-  rm $HOME/.tabletaliases                             ; ln .tabletaliases              $HOME/.tabletaliases
-  rm $HOME/.vimrc                                     ; ln .vimrc                      $HOME/.vimrc
-  rm $HOME/.XResources                                ; ln .XResources                 $HOME/.XResources
-  rm $HOME/.xmodmap                                   ; ln .xmodmap                    $HOME/.xmodmap
-  rm $HOME/.config/Code/User/settings.json            ; ln settings.json               $HOME/.config/Code/User/settings.json
-  rm $HOME/.config/Code/User/keybindings.json         ; ln keybindings.json            $HOME/.config/Code/User/keybindings.json
-  rm $HOME/.config/catt/catt.cfg                      ; ln catt.cfg                    $HOME/.config/catt/catt.cfg
-  rm $HOME/.ipython/profile_default/ipython_config.py ; ln ipython_config.py           $HOME/.ipython/profile_default/ipython_config.py
-  rm $HOME/.jupyter/custom/custom.js                  ; ln custom.js                   $HOME/.jupyter/custom/custom.js
+rm $HOME/.aliases
+ln .aliases $HOME/.aliases
+rm $HOME/.aliases.bash
+ln .aliases.bash $HOME/.aliases.bash
+rm $HOME/.colordiffrc
+ln .colordiffrc $HOME/.colordiffrc
+rm $HOME/.cshrc
+ln .cshrc $HOME/.cshrc
+rm $HOME/.cwdcmd
+ln .cwdcmd $HOME/.cwdcmd
+rm $HOME/.gitconfig
+ln .gitconfig $HOME/.gitconfig
+rm $HOME/.pdbrc
+ln .pdbrc $HOME/.pdbrc
+rm $HOME/.precmd
+ln .precmd $HOME/.precmd
+rm $HOME/.prompt
+ln .prompt $HOME/.prompt
+rm $HOME/.screen/default
+ln .screen/default $HOME/.screen/default
+rm $HOME/.screenrc
+ln .screenrc $HOME/.screenrc
+rm $HOME/.tabletaliases
+ln .tabletaliases $HOME/.tabletaliases
+rm $HOME/.vimrc
+ln .vimrc $HOME/.vimrc
+rm $HOME/.XResources
+ln .XResources $HOME/.XResources
+rm $HOME/.xmodmap
+ln .xmodmap $HOME/.xmodmap
+rm $HOME/.xsession
+ln .xsession $HOME/.xsession
+rm $HOME/.config/Code/User/settings.json
+ln settings.json $HOME/.config/Code/User/settings.json
+rm $HOME/.config/Code/User/keybindings.json
+ln keybindings.json $HOME/.config/Code/User/keybindings.json
+rm $HOME/.config/catt/catt.cfg
+ln catt.cfg $HOME/.config/catt/catt.cfg
+rm $HOME/.ipython/profile_default/ipython_config.py
+ln ipython_config.py $HOME/.ipython/profile_default/ipython_config.py
+rm $HOME/.jupyter/custom/custom.js
+ln custom.js $HOME/.jupyter/custom/custom.js
 
-  #rm $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml ; ln xfce4-panel.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
-  rm $HOME/scripts/updateall.sh  ; ln updateall.sh $HOME/scripts/updateall.sh
+#rm $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml ; ln xfce4-panel.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+rm $HOME/scripts/updateall.sh
+ln updateall.sh $HOME/scripts/updateall.sh
 
-  rm $HOME/.config/nvim/init.vim  ; ln init.vim $HOME/.config/nvim/init.vim
-  rm $HOME/.screenlayout/3monitor.sh  ; ln 3monitor.sh $HOME/.screenlayout/3monitor.sh
+rm $HOME/.config/nvim/init.vim
+ln init.vim $HOME/.config/nvim/init.vim
+rm $HOME/.screenlayout/3monitor.sh
+ln 3monitor.sh $HOME/.screenlayout/3monitor.sh
 
+# VS Code
+rm $HOME/.vscode/extensions/material-theme-jeffjose -r
+mkdir -p $HOME/.vscode/extensions
+cp -r material-theme-jeffjose $HOME/.vscode/extensions/material-theme-jeffjose
 
+# Install Python packages
+./pip.sh
 
-  # VS Code
-  rm $HOME/.vscode/extensions/material-theme-jeffjose -r
-  mkdir -p $HOME/.vscode/extensions
-  cp -r material-theme-jeffjose $HOME/.vscode/extensions/material-theme-jeffjose
+# Yarn packages
+./yarn.sh
 
-  # Install Python packages
-  ./pip.sh
+# VS Code packages
+./vscode.sh
 
-  # Yarn packages
-  ./yarn.sh
-
-  # VS Code packages
-  ./vscode.sh
-
-  # VIM Plugins
-  ./vim.sh
+# VIM Plugins
+./vim.sh
 
 # Ignore
 #
