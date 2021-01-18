d98b908bd7339953c3039673a1c55caec7782cd9
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-01-17 19:33:30 -0800

    Cleaning up. Deleting unused config files

--- 3monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-#xrandr --output DP-2-1 --mode 1920x1080 --pos 1080x0 --rotate right --output DP-2-2 --mode 1920x1080 --pos 0x0 --rotate left --output DP-2-3 --off --output eDP-1 --primary --mode 2560x1440 --pos 2160x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
