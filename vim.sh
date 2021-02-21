92c7f117bc420e541c77ecf1085b318accb6a567
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-02-21 00:33:19 -0800

    optimizing vim.sh

--- vim.sh
+++ vim.sh
@@ -17,17 +17,22 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vi
 # Setup vim plugins
 #
 #
-rm -rf ~/.vim/bundle/vim-vue
-git clone https://github.com/posva/vim-vue.git ~/.vim/bundle/vim-vue
 
-rm -rf ~/.vim/bundle/dart-vim-plugin
-git clone https://github.com/dart-lang/dart-vim-plugin ~/.vim/bundle/dart-vim-plugin
+clone() {
 
-rm -rf ~/.vim/bundle/vim-coffee-script
-git clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script
+  echo ""
+  echo "> Working on $1"
 
-rm -rf ~/.vim/bundle/vim-pug
-git clone git://github.com/digitaltoad/vim-pug.git ~/.vim/bundle/vim-pug
+  [ -d $2 ] || git clone $1 $2
+  (
+    cd $2
+    git pull $1
+  )
 
-rm -rf ~/.vim/bundle/vim-svelte-plugin
-git clone https://github.com/leafoftree/vim-svelte-plugin ~/.vim/bundle/vim-svelte-plugin
+}
+
+clone https://github.com/posva/vim-vue.git ~/.vim/bundle/vim-vue
+clone https://github.com/dart-lang/dart-vim-plugin ~/.vim/bundle/dart-vim-plugin
+clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script
+clone git://github.com/digitaltoad/vim-pug.git ~/.vim/bundle/vim-pug
+clone https://github.com/leafoftree/vim-svelte-plugin ~/.vim/bundle/vim-svelte-plugin
