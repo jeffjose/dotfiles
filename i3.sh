26cb933de1fe773e1a760c6677853905db1165e8
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-02-21 01:09:26 -0800

    perf optimization for i3.sh

--- i3.sh
+++ i3.sh
@@ -17,5 +17,18 @@ PACKAGES=(
   lxappearance
 )
 
-# Run
-sudo apt install ${PACKAGES[*]}
+install() {
+  for i in $@; do
+    if [ -z "$(apt-cache show $i 2>/dev/null)" ]; then
+      echo " > package $i not available on repo."
+    else
+      echo " > add package $i to the install list"
+      packages="$packages $i"
+    fi
+  done
+  sudo apt update
+  sudo apt install -y $packages
+  sudo apt autoremove
+}
+
+install "${packages[@]}"
