8cac67dcba7f9e8af9a5e8bf94d4dc13c7f58752
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-06-12 17:35:38 -0700

    always install current version of node

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -4,7 +4,7 @@
 #
 # NodeJS
 
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt install -y nodejs
 
 sudo apt install -y g++ build-essential
