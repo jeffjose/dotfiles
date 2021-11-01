24e17337888ca1c7f8e2db8772dff370c84d9ecb
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-11-01 11:19:03 -0700

    no need to update conda anymore

--- updateall.sh
+++ updateall.sh
@@ -24,22 +24,6 @@ echo -e '---------------------------------------------------------'
 echo -e ''
 yarn global upgrade all
 
-echo -e ''
-echo -e '---------------------------------------------------------'
-echo -e 'Updating python3 conda packages'
-echo -e '---------------------------------------------------------'
-echo -e ''
-
-source activate root
-# Update conda itself
-conda update -n base conda --yes
-
-# Update conda
-conda update --all --yes
-
-# Cleanup conda
-conda clean --all --yes
-
 # Flutter
 flutter upgrade
 
