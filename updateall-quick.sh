e73fbc159d882e2f2d961408ea4ecb4535b4acea
Jeffrey Jose <jeffjosejeff@gmail.com>
2024-09-14 09:13:12 -0700

    newline

--- updateall-quick.sh
+++ updateall-quick.sh
@@ -5,7 +5,14 @@
 
 sudo true
 
+echo ""
 $HOME/dotfiles/update_vim.sh
+
+echo ""
 $HOME/dotfiles/update_code.sh
+
+echo ""
 $HOME/dotfiles/update_chrome.sh
+
+#echo ""
 #$HOME/dotfiles/update_rust.sh
