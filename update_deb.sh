10bb0a26608bf368f573addecb9321f777fc8361
Jeffrey Jose <jeffjosejeff@gmail.com>
2026-01-15 08:38:41 -0800

    Cache sudo credentials upfront to avoid mixed output

--- update_deb.sh
+++ update_deb.sh
@@ -66,6 +66,9 @@ build_command() {
 echo "Updating deb packages..."
 echo ""
 
+# Cache sudo credentials upfront to avoid password prompts during parallel downloads
+sudo -v
+
 # Read packages into arrays
 declare -a pkg_names=()
 declare -a pkg_types=()
